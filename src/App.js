302c19902c11616c96867ebef97e11920859fde2
Eric Zhang <ekzhang1@gmail.com>
2019-03-01 14:09:29 -0600

    Add undo button

--- src/App.js
+++ src/App.js
@@ -14,37 +14,53 @@ function startState() {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = startState();
+    this.state = { history: [startState()] };
     this.restart = this.restart.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.aiMove = this.aiMove.bind(this);
+    this.undo = this.undo.bind(this);
+  }
+
+  get gameState() {
+    return this.state.history[this.state.history.length - 1];
+  }
+
+  set gameState(next) {
+    this.setState({
+      history: this.state.history.concat([next])
+    });
   }
 
   restart() {
-    this.setState(startState());
+    this.setState({ history: [startState()] });
+  }
+
+  undo() {
+    this.setState({ history: this.state.history.slice(0, -1) });
   }
 
   handleClick(i, j) {
-    if (findGlobalWinner(this.state.board) !== -1)
+    const { gameState } = this;
+    if (findGlobalWinner(gameState.board) !== -1)
       return; // game has ended
-    if (this.state.board[i][j] !== -1)
+    if (gameState.board[i][j] !== -1)
       return; // cell already occupied
-    if (this.state.current !== -1) {
-      const local = this.state.board[this.state.current];
+    if (gameState.current !== -1) {
+      const local = gameState.board[gameState.current];
       const localWinner = findWinner(local);
       const freeSquares = local.indexOf(-1) !== -1;
-      if (localWinner === -1 && freeSquares && i !== this.state.current)
+      if (localWinner === -1 && freeSquares && i !== gameState.current)
         return; // must move in designated sub-board if possible
-      else if (localWinner !== -1 && i === this.state.current)
+      else if (localWinner !== -1 && i === gameState.current)
         return; // cannot move in won board
     }
-    const board2 = copyArray(this.state.board);
-    board2[i][j] = this.state.turn;
-    this.setState({
+    const board2 = copyArray(gameState.board);
+    board2[i][j] = gameState.turn;
+    this.gameState = {
       board: board2,
-      turn: 1 - this.state.turn,
+      turn: 1 - gameState.turn,
       current: j
-    });
+    };
     const winner = findGlobalWinner(board2);
     if (winner !== -1) {
       setTimeout(() => {
@@ -54,7 +70,7 @@ class App extends Component {
   }
 
   aiMove() {
-    const move = nextMove(this.state);
+    const move = nextMove(this.gameState);
     if (move) {
       this.handleClick(...move);
     }
@@ -65,12 +81,12 @@ class App extends Component {
       <div className="App">
         <h1>Super Tic-Tac-Toe</h1>
         <div className="grid">
-          <Board board={this.state.board} onClick={this.handleClick} />
+          <Board board={this.gameState.board} onClick={this.handleClick} />
         </div>
         <div className="controls">
           <button onClick={this.restart}>Restart</button>{' '}
-          <button onClick={this.aiMove}>AI Move</button>
-          {/* <button>Undo</button> */}
+          <button onClick={this.aiMove}>AI Move</button>{' '}
+          <button onClick={this.undo}>Undo</button>
         </div>
       </div>
     );
--- src/Board.js
+++ src/Board.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SubBoard from './SubBoard';
 
-class Board extends Component {
-  render() {
-    return (
-      <table style={{ background: 'black' }}>
-        <tbody>
-          <tr>
-            <td><SubBoard value={this.props.board[0]} onClick={j => this.props.onClick(0, j)} /></td>
-            <td><SubBoard value={this.props.board[1]} onClick={j => this.props.onClick(1, j)} /></td>
-            <td><SubBoard value={this.props.board[2]} onClick={j => this.props.onClick(2, j)} /></td>
-          </tr>
-          <tr>
-            <td><SubBoard value={this.props.board[3]} onClick={j => this.props.onClick(3, j)} /></td>
-            <td><SubBoard value={this.props.board[4]} onClick={j => this.props.onClick(4, j)} /></td>
-            <td><SubBoard value={this.props.board[5]} onClick={j => this.props.onClick(5, j)} /></td>
-          </tr>
-          <tr>
-            <td><SubBoard value={this.props.board[6]} onClick={j => this.props.onClick(6, j)} /></td>
-            <td><SubBoard value={this.props.board[7]} onClick={j => this.props.onClick(7, j)} /></td>
-            <td><SubBoard value={this.props.board[8]} onClick={j => this.props.onClick(8, j)} /></td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
+function Board({ board, onClick }) {
+  return (
+    <table style={{ background: 'black' }}>
+      <tbody>
+        <tr>
+          <td><SubBoard value={board[0]} onClick={j => onClick(0, j)} /></td>
+          <td><SubBoard value={board[1]} onClick={j => onClick(1, j)} /></td>
+          <td><SubBoard value={board[2]} onClick={j => onClick(2, j)} /></td>
+        </tr>
+        <tr>
+          <td><SubBoard value={board[3]} onClick={j => onClick(3, j)} /></td>
+          <td><SubBoard value={board[4]} onClick={j => onClick(4, j)} /></td>
+          <td><SubBoard value={board[5]} onClick={j => onClick(5, j)} /></td>
+        </tr>
+        <tr>
+          <td><SubBoard value={board[6]} onClick={j => onClick(6, j)} /></td>
+          <td><SubBoard value={board[7]} onClick={j => onClick(7, j)} /></td>
+          <td><SubBoard value={board[8]} onClick={j => onClick(8, j)} /></td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 export default Board;
